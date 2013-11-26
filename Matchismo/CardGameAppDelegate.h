e3f6109b968c00df562065f4a6c78ff5f53d83bf
Leonid Batizhevsky <the.leonko@gmail.com>
2013-11-26 20:35:22 +0400

    Initial Commit

---
+++ Matchismo/CardGameAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CardGameAppDelegate.h
+//  Matchismo
+//
+//  Created by Leonid Batizhevsky on 26.11.13.
+//  Copyright (c) 2013 Leonid Batizhevsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Leonid Batizhevsky on 26.11.13.
+//  Copyright (c) 2013 Leonid Batizhevsky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
